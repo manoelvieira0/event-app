06c5b97eb47fb6b703670dd3a6599c60924baf73
Manoel Vieira <netinhovieira1801@gmail.com>
2023-11-13 14:46:48 -0300

    Utilizando StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,25 +1,31 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      padding: 24,
-      backgroundColor: '#131016'
-    }}>
-      <Text style={{
-        color: '#FDFCFF',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>Nome do evento</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
 
-      <Text style={{
-        color: '#6b6b6b',
-        fontSize: 16,
-      }}>
+      <Text style={styles.eventDate}>
         Sexta, 17 de novembro de 2023
       </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#131016'
+  },
+  eventName: {
+    color: '#FDFCFF',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  }
+})
