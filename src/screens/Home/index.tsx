887771c2cc99b7d22d2aec1a66c2e027b69dab3c
Manoel Vieira <netinhovieira1801@gmail.com>
2023-11-13 14:52:00 -0300

    Organizando o projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 17 de novembro de 2023
+      </Text>
+    </View>
+  );
+}
